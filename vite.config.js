2aefc386394efdcdc96a8e11df5ffb81346170f2
Ariya Mortazavian <s.mortazaviannajafabadi@vikes.csuohio.edu>
2025-04-10 15:51:07 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    host: "localhost",
-    port: 3000,
-    hmr: {
-      overlay: false,  // Disable the error overlay
-    },
-  },
-})
+  base: '/Website-/', // Ensure this matches your repository name
+});
